e4ad6ce997767eda45f38277018465e86650b194
Tommi Virtanen <tv@inoi.fi>
2006-01-02 17:07:33 +0200

    Add a del.icio.us export parsing bookshelf implementation.

---
+++ delicate/delicious/fromdelicious.py
@@ -0,0 +1,98 @@
+"""Parse bookmarks exported as HTML from del.icio.us."""
+
+from zope.interface import implements
+import datetime
+from twisted.web import microdom
+
+from delicate import ibookshelf, bookmark
+
+class DeliciousBookmarkShelf(object):
+    implements(ibookshelf.IBookmarkShelf)
+
+    def __init__(self, filename):
+        self.filename = filename
+
+    def _timestamp(self, seconds):
+        return datetime.datetime.fromtimestamp(int(seconds))
+
+    def _getText_2(self, nodes):
+        for node in nodes:
+            if isinstance(node, microdom.Text):
+                yield node.value.decode('utf-8')
+            elif isinstance(node, microdom.EntityReference):
+                found = None
+                for char, xml in microdom.REV_HTML_ESCAPE_CHARS:
+                    if xml == ('&%s;' % node.eref):
+                        found = char
+                        break
+
+                if found is not None:
+                    yield found.decode('utf-8')
+                else:
+                    raise RuntimeError('Unknown entity reference: %s' % node.eref)
+            else:
+                raise RuntimeError('Not a text node: %r' % node)
+
+    def _getText(self, nodes):
+        return u''.join(self._getText_2(nodes))
+
+    def _nodeToBookmark(self, dt):
+        assert dt.nodeName == 'dt'
+        i = dt.parentNode.childNodes.index(dt)
+        try:
+            dd = dt.parentNode.childNodes[i+1]
+        except IndexError:
+            dd = None
+        else:
+            assert dd.nodeName == 'dd'
+
+        links = dt.getElementsByTagName('a')
+        assert len(links) == 1
+        a = links[0]
+
+        assert len(a.childNodes) >= 1
+        bm = bookmark.Bookmark(
+            url=a.getAttribute('href', '').decode('utf-8'),
+            title=self._getText(a.childNodes),
+            created=self._timestamp(a.getAttribute('add_date')),
+            modified=self._timestamp(a.getAttribute('last_modified')),
+            tags=a.getAttribute('tags', '').decode('utf-8').split(None))
+
+        if dd is None:
+            desc = None
+        else:
+            desc = self._getText(dd.childNodes)
+
+            suffixes = [
+                u' (tags:  %s)\n' % u' '.join(bm.tags),
+                u'\n(tags: %s)\n' % u' '.join(bm.tags),
+                ]
+            for suffix in suffixes:
+                if desc.endswith(suffix):
+                    desc = desc[:-len(suffix)]
+                    break
+            if desc == '':
+                desc = None
+        bm.description = desc
+        return bm
+
+    def get(self, url):
+        tree = microdom.parse(self.filename, beExtremelyLenient=True)
+        for dl in tree.getElementsByTagName('dl'):
+            for a in dl.getElementsByTagName('a'):
+                if a.getAttribute('href') == url:
+                    if a.parentNode.nodeName == 'dt':
+                        return self._nodeToBookmark(a.parentNode)
+
+    def __iter__(self):
+        # workaround for http://twistedmatrix.com/bugs/issue1358
+        laterClosers = {}
+        laterClosers.update(microdom.MicroDOMParser.laterClosers)
+        laterClosers['p'] = microdom.MicroDOMParser.laterClosers.get('p', []) + ['DT']
+        laterClosers['dt'] = microdom.MicroDOMParser.laterClosers.get('dt', []) + ['DD']
+        laterClosers['dd'] = microdom.MicroDOMParser.laterClosers.get('dd', []) + ['DT']
+
+        tree = microdom.parse(self.filename, beExtremelyLenient=True, laterClosers=laterClosers)
+        for dl in tree.getElementsByTagName('dl'):
+            for dt in dl.getElementsByTagName('dt'):
+                yield self._nodeToBookmark(dt)
---
+++ delicate/delicious/test/test_fromdelicious.py
@@ -0,0 +1,60 @@
+from twisted.trial import unittest
+from twisted.python import util
+from cStringIO import StringIO
+import datetime
+from delicate.delicious import fromdelicious
+
+class FromDelicious(unittest.TestCase):
+    def test_simple(self):
+        # export.html is hand-crafted based on a page save
+        # mangled by Firefox
+        filename = util.sibpath(__file__, 'export.html')
+        shelf = fromdelicious.DeliciousBookmarkShelf(filename)
+
+        self.assertEquals(len(list(iter(shelf))), 3)
+
+        notfound = shelf.get('http://not-there/')
+        self.assertIdentical(notfound, None)
+
+        a = shelf.get('http://www.example.com/url1')
+        self.assertEquals(a.url, 'http://www.example.com/url1')
+        self.assertEquals(a.title, 'Title one is pretty vague')
+        self.assertIdentical(a.description, None)
+        self.assertEquals(a.created,
+                          datetime.datetime(2005, 12, 10, 19, 41, 38))
+        self.assertEquals(a.modified,
+                          datetime.datetime(2005, 12, 10, 19, 41, 38))
+        self.assertEquals(a.tags, ['foo', 'bar'])
+
+        b = shelf.get('http://example.org/')
+        self.assertEquals(b.url, 'http://example.org/')
+        self.assertEquals(b.title, 'Second link title & more here')
+        self.assertIdentical(b.description, None)
+        self.assertEquals(b.created,
+                          datetime.datetime(2005, 12, 7, 18, 12, 16))
+        self.assertEquals(b.modified,
+                          datetime.datetime(2005, 12, 7, 18, 12, 16))
+        self.assertEquals(b.tags, ['system:unfiled'])
+
+        c = shelf.get('http://third.one.invalid/')
+        self.assertEquals(c.url, 'http://third.one.invalid/')
+        self.assertEquals(c.title, 'Third title')
+        self.assertEquals(c.description, '''This item has a very very, very, very long description
+that goes on and on and on and on and on and on and on & on
+but actually del.icio.us seems to truncate them at some point which is really stup ...''')
+        self.assertEquals(c.created,
+                          datetime.datetime(2005, 12, 5, 18, 29, 29))
+        self.assertEquals(c.modified,
+                          datetime.datetime(2005, 12, 5, 18, 29, 29))
+        self.assertEquals(c.tags, ['xyzzy', 'foo'])
+
+    def test_dialtone(self):
+        # export.html received from dialtone, apparently without
+        # any browser mangling
+        filename = util.sibpath(__file__, 'export-dialtone.html')
+        shelf = fromdelicious.DeliciousBookmarkShelf(filename)
+
+        self.assertEquals(len(list(iter(shelf))), 45)
+
+        notfound = shelf.get('http://not-there/')
+        self.assertIdentical(notfound, None)
