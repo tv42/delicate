3c779f82623bd6b6e6d8c8e462511c5ca7436832
Tommi Virtanen <tv@inoi.fi>
2006-01-02 17:09:26 +0200

    Add command "deli2dir" to convert delicious export.html to a directory tree.

---
+++ delicate/commands/deli2dir.py
@@ -0,0 +1,27 @@
+import sys, os
+from twisted.python import usage
+from zope.interface import implements
+from delicate import bookshelf
+from delicate.delicious import fromdelicious
+from delicate.usage import ICommand
+
+class Deli2Dir(usage.Options):
+    """
+    Convert delicious export.html to a directory tree.
+
+    The export.html file must be UTF-8 encoded.
+    """
+    implements(ICommand)
+
+    def parseArgs(self, export, path):
+	self['export'] = export
+	self['path'] = path
+
+    def postOptions(self):
+        os.mkdir(self['path'])
+        src = fromdelicious.DeliciousBookmarkShelf(self['export'])
+        dst = bookshelf.FileBookshelf(self['path'])
+        for bookmark in src:
+            dst.add(bookmark)
+
+x = Deli2Dir()
